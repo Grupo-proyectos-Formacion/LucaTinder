5afa579aec18665854dd8d7525a13a9f1f444682
JulianBautista <julian.910606@gmail.com>
2019-10-17 12:42:28 +0200

    Proyecto creado

---
+++ MVC_REST/lucatinder/src/main/java/com/grupo2/lucatinder/LucatinderApplication.java
@@ -0,0 +1,13 @@
+package com.grupo2.lucatinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucatinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucatinderApplication.class, args);
+	}
+
+}
---
+++ MVC_REST/lucatinder/src/test/java/com/grupo2/lucatinder/LucatinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.lucatinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucatinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
