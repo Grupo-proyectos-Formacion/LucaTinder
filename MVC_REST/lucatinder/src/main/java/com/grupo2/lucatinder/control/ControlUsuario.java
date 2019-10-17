55a3c262e682f1681c7c4b48ac43d58bba009512
JulianBautista <julian.910606@gmail.com>
2019-10-17 16:17:19 +0200

    control Usuario modificado

--- MVC_REST/lucatinder/src/main/java/com/grupo2/lucatinder/control/ControlUsuario.java
+++ MVC_REST/lucatinder/src/main/java/com/grupo2/lucatinder/control/ControlUsuario.java
@@ -2,7 +2,10 @@ package com.grupo2.lucatinder.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.grupo2.lucatinder.model.Usuario;
 import com.grupo2.lucatinder.service.ServiceGenerico;
@@ -14,10 +17,15 @@ public class ControlUsuario {
 	private ServiceGenerico<Usuario> service;
 	
 	@GetMapping("/crear/usuario")
-	public String crear(Usuario usuario){
+	public String crear(Model model){
+		model.addAttribute("usuario", new Usuario());
+		return "/crearUsuario";
 		
+	}
+	
+	@PostMapping("/crear/usuario")
+	public String guardar(@ModelAttribute Usuario usuario){
 		service.crear(usuario);
-		
 		return "/altaUsuario";
 		
 	}
