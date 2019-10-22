b4179d57131c7f548a80c8d04b49d96c41647ece
JulianBautista <julian.910606@gmail.com>
2019-10-22 20:01:21 +0200

    form creada

--- Angular/lucatinder/src/app/components/alta-usuario/alta-usuario.component.ts
+++ Angular/lucatinder/src/app/components/alta-usuario/alta-usuario.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UsuarioService } from '../../services/usuario.service'
+import { Usuario } from '../../models/usuario'
 
 @Component({
   selector: 'app-alta-usuario',
@@ -7,10 +9,29 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./alta-usuario.component.css']
 })
 export class AltaUsuarioComponent implements OnInit {
-  constructor() { }
-
+  form:FormGroup;
+  user:Usuario;
+  constructor(private service:UsuarioService, private fb:FormBuilder) {
+    this.createForm();
+   }
   ngOnInit() {
-
+   // this.form = this.fb.group(this.user)
+   
   }
+
+  createForm() {
+    this.form = this.fb.group({
+      //name: ['', Validators.required],
+      nombreUsuario: ['', Validators.required],
+      descripcionUsuario : "",
+      edadUsuario: "",
+      ciudadUsuario: "",
+      imagenUsuario: "",
+    });
+  }
+
+  /*altaUsuario(){
+    this.service.crearUsuario(this.user);
+  }*/
 
 }
