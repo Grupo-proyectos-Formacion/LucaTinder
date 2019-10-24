ec2c28d98a850105e7022d3b4ff44f8a76b60a04
Daniel Fernandez <danifhid@gmail.com>
2019-10-24 11:58:44 +0200

    tomaaa

--- Angular/lucatinder/src/app/components/login/login.component.ts
+++ Angular/lucatinder/src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/Usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  usuario:string;
+  form: any;
+
+
+  constructor(private service:UsuarioService,  private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,) { }
 
   ngOnInit() {
-    
+   
   }
-
+  createF.
+  get nombreUsuario(){return this.form.get('nombreUsuario');}
+  logIn(){
+    if(this.usuario == ){
+      this.router.navigate(["user"]);
+     }else {
+       alert("Invalid credentials");
+  }
 }
