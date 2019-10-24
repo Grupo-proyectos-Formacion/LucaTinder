42f749d1a6cf19e2868d536ffc8a82202890a57f
Luis Pedraza Soldado <luisp.soldado@gmail.com>
2019-10-24 21:59:03 +0200

    mvc finalizado

---
+++ MVC_REST/lucatinder/src/main/resources/static/js/propio.js
@@ -0,0 +1,32 @@
+/*SCROLL REVEAL*/	
+	window.sr = ScrollReveal();
+	sr.reveal('.navbar', {
+	  duration:2000,
+	  origin:'top',
+	  distance: '200px',
+	  delay:'2000'
+	});
+	window.sr = ScrollReveal();
+	sr.reveal('.luca-tinder', {
+	  duration:3000,
+	  origin:'right',
+	  distance: '900px'
+	});
+	window.sr = ScrollReveal();
+	sr.reveal('#about', {
+	  duration:2000,
+	  origin:'bottom'
+	});
+	window.sr = ScrollReveal();
+	sr.reveal('#fila1', {
+	  duration:2000,
+	  origin:'left',
+	  distance: '900px'
+	});
+	window.sr = ScrollReveal();
+	sr.reveal('#fila2', {
+	  duration:2000,
+	  origin:'right',
+	  distance: '900px'
+	});
+	
